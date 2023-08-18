b7a99874057205f638299b9a4e24c1e80331ab74
严泉美 <yanquanmei@yanquanmeideMacBook-Pro.local>
2023-08-18 12:37:40 +0800

    feat: 删除不必要的注释，增加tests文件内容

--- demo/simple.ts
+++ demo/simple.ts
@@ -1,4 +1,8 @@
 /* vue 或 react项目 */
 // 简单示例
-import WebsocketClient from "websocket-client";
-const socket = new WebsocketClient("ws://127.0.0.1:3001");
+import Socket from "websocket-front";
+const socket = new Socket("ws://127.0.0.1:3001");
+
+setTimeout(() => {
+  socket.send("hello world");
+}, 1000);
